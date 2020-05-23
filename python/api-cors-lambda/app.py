57daefe9a470ff9ac137cff5bb3c57b0c32c6016
Max Gorbatenko <max@musclecomputing.com>
2020-05-22 19:42:40 -0500

    Update app.py

--- python/api-cors-lambda/app.py
+++ python/api-cors-lambda/app.py
@@ -10,25 +10,29 @@ class ApiCorsLambdaStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        base_lambda = _lambda.Function(self,'ApiCorsLambda',
-        handler='lambda-handler.handler',
-        runtime=_lambda.Runtime.PYTHON_3_7,
-        code=_lambda.Code.asset('lambda'),
+        base_lambda = _lambda.Function(
+            self,'ApiCorsLambda',
+            handler='lambda-handler.handler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
         )
         
-        base_api = _apigw.RestApi(self, 'ApiGatewayWithCors',
-        rest_api_name='ApiGatewayWithCors')
+        base_api = _apigw.RestApi(
+            self, 'ApiGatewayWithCors',
+            rest_api_name='ApiGatewayWithCors',
+        )
 
         example_entity = base_api.root.add_resource('example')
-        example_entity_lambda_integration = _apigw.LambdaIntegration(base_lambda,proxy=False, integration_responses=[
-            {
-                'statusCode': '200',
-                'responseParameters': {
-                'method.response.header.Access-Control-Allow-Origin': "'*'",
+        example_entity_lambda_integration = _apigw.LambdaIntegration(
+            base_lambda, proxy=False, integration_responses=[
+                {
+                    'statusCode': '200',
+                    'responseParameters': {
+                        'method.response.header.Access-Control-Allow-Origin': "'*'",
+                    }
                 }
-            }
-                ]
-            )
+            ]
+        )
         example_entity.add_method('GET', example_entity_lambda_integration, 
                 method_responses=[{
                     'statusCode': '200',
